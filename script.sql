49c3f80dfe29fafbd4e4a73b40c3ff501534586d
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2025-02-27 08:55:12 +0100

    Create pays_continent table and import data from CSV; add queries for continent count and short country names

--- script.sql
+++ script.sql
@@ -1,7 +1,17 @@
-COPY gamenn
-FROM '/home/etudiant/Documents/design_tp_games.csv'
+CREATE TABLE pays_continent (
+    country VARCHAR(50) PRIMARY KEY,
+    continent VARCHAR(50)
+);
+
+
+COPY pays_continent(continent, country)
+FROM '/home/etudiant/Documents/Cours/R4.C.10/Countries-Continents.csv'
 WITH (
   FORMAT csv,
   HEADER true,
-  DELIMITER ';'
+  DELIMITER ','
 );
+
+SELECT continent, COUNT(country) from pays_continent GROUP BY continent;
+
+SELECT country from pays_continent WHERE LENGTH(country) < 5;
